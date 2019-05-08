4220f6365fe0454b5ea95c3f38eb1f43c37cea35
bt-zymoresearch <btran@zymoresearch.com>
2019-05-08 09:51:19 -0700

    Create part of crud

---
+++ CRUD_Razor_2_1/Pages/BookList/index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRUD_Razor_2_1.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Razor_2_1.Pages.BookList
+{
+    public class indexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IEnumerable<Book> Books { get; set; }
+
+        public indexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+            
+        public async Task OnGet()
+        {
+            Books = await _db.Books.ToListAsync();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # musical-intention
 
+```
+# Package Manager Console
+
+# CMD <Meaning name of migration>
+PM> add-migration AddBookToDatabase
+
+```
+
+```
+# Microsoft SQL Server Management Studio
+
+Server name: (LocalDb)\MSSQLLocalDB
+```
